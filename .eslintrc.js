9e35d2192e5b14740cee0c5d2c3ca9aa93c08992
LoicZHU <zhu.loic@gmail.com>
2024-06-04 18:15:06 +0200

    chore: add multiple configuration files

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  plugins: ['angular', 'unused-imports'],
+  extends: [
+    'eslint:recommended',
+    'plugin:angular/johnpapa',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'angular/controller-as-vm': ['error', 'never'],
+    semi: ['error', 'always'],
+    'no-console': 'warn',
+    'no-unused-vars': ['error', { args: 'none' }],
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+};
